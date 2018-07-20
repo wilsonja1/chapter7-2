fa5b5d6a4da2bbc2dcfbcb478a05acc1c6d7c1bd
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 13:04:51 -0700

    Add project files.

---
+++ chapter7-2/Location.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter7_2
+{
+    abstract class Location
+    {
+        public string Name { get; private set; }
+
+        public Location(string name)
+        {
+            Name = name;
+        }
+
+        public Location[] Exits;
+
+        public virtual string Description
+        {
+            get
+            {
+                string description = "You're standing in the " + Name + ". You see exits to teh following places: ";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += " " + Exits[i].Name;
+                    if (i != Exits.Length - 1)
+                    {
+                        description += ",";
+                    }
+                }
+                return description;
+            }
+        }
+
+
+    }
+}
