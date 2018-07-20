75de27bf2966b58ba605d63e61d8dfd01a5b9aa1
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 13:07:48 -0700

    Filled in exterior door interface

---
+++ chapter7-2/IHasExteriorDoor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter7_2
+{
+    interface IHasExteriorDoor
+    {
+        string DoorDescription { get; }
+        Location DoorLocation { get; set; }
+    }
+}
